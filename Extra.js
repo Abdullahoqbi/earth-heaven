2c9494f222ed9bba24d3607cd2ffbfb27a59af16
abdullahoqbi <bode_oqbi@hotmail.com>
2021-01-28 05:05:12 -0800

    extra.js

---
+++ Extra.js
@@ -0,0 +1,19 @@
+var cultivationtype=prompt("what sort of plant do you prefer?");
+var insectkillertype=prompt("what sort of plant do you prefer?");
+
+var orderdetail;
+
+if(insectkiller > 1 && insectkiller <3){
+orderdetail="congrats you gained a one bottle more"
+}else if (insectkiller >3 && insectkiller <=5){
+orderdetail= "congrats you gained a two bottle more";
+
+orderdetail="sorry your input incorrect";
+
+
+
+
+
+
+
+document.write (orderdetail)
