430ddfce95ecf6653547a3f3aa5aea828f060cf0
abdullahoqbi <bode_oqbi@hotmail.com>
2021-01-30 12:15:00 -0800

    Extra.js

--- Extra.js
+++ Extra.js
@@ -1,14 +1,24 @@
-var insectkiller = prompt("How many KG of orange do you want?");
+var Fruit = prompt("How many KG of orange do you want?");
 var orderdetail = "";
-
-if (insectkiller > 1 && insectkiller < 5) {
-    orderdetail = "congrats you gained a one KG more"
+while (Fruit != 2 && Fruit != 3) {
+    Fruit = prompt("How many KG of orange do you want?");
+}
+if (Fruit == 2) {
+    orderdetail = "congrats you gained a one KG more";
 }
 else {
-    if (insectkiller >= 5 && insectkiller < 10) {
+    if (Fruit == 3) {
         orderdetail = "congrats you gained a two KG more";
     }
 }
+var picture = prompt("How many picture do you want to print?");
+var sum = ""
+var pic = '<img src="orange.jpg"/>'
+for (var i = 0; i < picture; i++) {
+    sum = sum + pic;
+}
+
 confirm("Are you sure??");
 alert("Thank you for using this page!");
 document.write(orderdetail);
+document.write(sum);
