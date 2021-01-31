22e68b16b6c4a372959555380bdcda1adebe8fd3
abdullahoqbi <bode_oqbi@hotmail.com>
2021-01-31 04:54:44 -0800

    index.js

--- Extra.js
+++ Extra.js
@@ -11,14 +11,15 @@ else {
         orderdetail = "congrats you gained a two KG more";
     }
 }
-var picture = prompt("How many picture do you want to print?");
-var sum = ""
-var pic = '<img src="orange.jpg"/>'
-for (var i = 0; i < picture; i++) {
-    sum = sum + pic;
-}
+function fruit() {
+    var picture = prompt("How many picture do you want to print?");
+    var sum = ""
+    var pic = '<img src="orange.jpg"/>'
+    for (var i = 0; i < picture; i++) {
+        sum = sum + pic;
+    }
 
-confirm("Are you sure??");
-alert("Thank you for using this page!");
-document.write(orderdetail);
-document.write(sum);
+    confirm("Are you sure??");
+    alert("Thank you for using this page!");
+    document.write(orderdetail);
+    document.write(sum);
